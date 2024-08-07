871e975be97e1810d8b3c745c189894cc47251a3
AhanafTahmid <ahanaftahmid55@gmail.com>
2024-08-08 03:43:07 +0600

    .

--- contest/Codeforces Round 964 (Div. 4)/G_1_Ruler_easy_version.cpp
+++ contest/Codeforces Round 964 (Div. 4)/G_1_Ruler_easy_version.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 void solve(){
     int l = 1, r = 1001, m;
-    for(int i=0;i<7;i++){
+    for(int i=0;i<10;i++){
         m = (l+r)>>1;
-        printf("? %lld %lld\n\n",1LL, m);
+        printf("? %lld %lld\n\n",m, m);
         int s;cin>>s;
-        if(m==s)l = m + 1;
+        if(m*m==s)l = m;
         else r = m;
         if (l == r) {
             cout << "! " << l << endl;
